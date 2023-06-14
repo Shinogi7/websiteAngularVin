a3f81ae08afee7e27fe2f92a1db26f763a07d08a
Shinogi7 <thomas.giresse@viacesi.fr>
2023-06-14 10:09:50 +0200

    10h09 14/06

--- websiteVin/src/app/app.module.ts
+++ websiteVin/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { ProduitComponent } from './components/produit/produit.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProduitComponent,
+    DetailComponent,
+    CardComponent,
+    ButtonComponent
+   
   ],
   imports: [
     BrowserModule,
